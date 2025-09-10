b403996315c9f57d1ca2236d6d3d7ec3482a98ec
rashik <mhdrashik112@gmail.com>
2025-09-10 17:38:25 +0530

    Test

--- app.py
+++ app.py
@@ -171,8 +171,9 @@ def create_app() -> Flask:
 
     return app
 
+app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+
+
